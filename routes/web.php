d8132249fae2516d9692c3657f59e9315cc4e4ab
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-06-06 17:47:03 +0700

    add new routes for categories

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PageController;
@@ -38,6 +39,7 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register.s
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware('auth');
 
 // ADMIN AUTHORIZATION START
+// -- MEMBER --
 Route::get('/dashboard/members', [MemberController::class, 'index'])->name('members.index')->middleware('admin');
 
 Route::get('/dashboard/members/create', [MemberController::class, 'create'])->name('members.create')->middleware('admin');
@@ -52,6 +54,7 @@ Route::put('/dashboard/members/{user:username}', [MemberController::class, 'upda
 
 Route::delete('/dashboard/members/{user:username}', [MemberController::class, 'destroy'])->name('members.destroy')->middleware('admin');
 
+// -- AUTHOR --
 Route::get('/dashboard/authors', [AuthorController::class, 'index'])->name('authors.index')->middleware('admin');
 
 Route::get('/dashboard/authors/create', [AuthorController::class, 'create'])->name('authors.create')->middleware('admin');
@@ -66,6 +69,7 @@ Route::put('/dashboard/authors/{author:author_code}', [AuthorController::class, 
 
 Route::delete('/dashboard/authors/{author:author_code}', [AuthorController::class, 'destroy'])->name('authors.destroy')->middleware('admin');
 
+// -- PUBLISHER --
 Route::get('/dashboard/publishers', [PublisherController::class, 'index'])->name('publishers.index')->middleware('admin');
 
 Route::get('/dashboard/publishers/create', [PublisherController::class, 'create'])->name('publishers.create')->middleware('admin');
@@ -79,4 +83,19 @@ Route::get('/dashboard/publishers/{publisher:publisher_code}/edit', [PublisherCo
 Route::put('/dashboard/publishers/{publisher:publisher_code}', [PublisherController::class, 'update'])->name('publishers.update')->middleware('admin');
 
 Route::delete('/dashboard/publishers/{publisher:publisher_code}', [PublisherController::class, 'destroy'])->name('publishers.destroy')->middleware('admin');
+
+// -- CATEGORY --
+Route::get('/dashboard/categories', [CategoryController::class, 'index'])->name('categories.index')->middleware('admin');
+
+Route::get('/dashboard/categories/create', [CategoryController::class, 'create'])->name('categories.create')->middleware('admin');
+
+Route::post('/dashboard/categories', [CategoryController::class, 'store'])->name('categories.store')->middleware('admin');
+
+Route::get('/dashboard/categories/{category:category_code}', [CategoryController::class, 'show'])->name('categories.show')->middleware('admin');
+
+Route::get('/dashboard/categories/{category:category_code}/edit', [CategoryController::class, 'edit'])->name('categories.edit')->middleware('admin');
+
+Route::put('/dashboard/categories/{category:category_code}', [CategoryController::class, 'update'])->name('categories.update')->middleware('admin');
+
+Route::delete('/dashboard/categories/{category:category_code}', [CategoryController::class, 'destroy'])->name('categories.destroy')->middleware('admin');
 // ADMIN AUTHORIZATION END
