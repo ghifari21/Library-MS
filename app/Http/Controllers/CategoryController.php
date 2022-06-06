7b81c5bff13eb269df4349766665d0a116ee00b2
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-06-06 17:47:52 +0700

    add search feature in Category Model and new CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bibliography;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard.admin.categories.index', [
+            'categories' => Category::filter(request(['search']))->orderBy('category_code', 'ASC')->paginate(25)->withQueryString()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255|unique:categories'
+        ]);
+
+        $category = Category::latest()->first();
+        if ($category) {
+            $validatedData['category_code'] = 'C' . $category->id + 1;
+        } else {
+            $validatedData['category_code'] = 'C1';
+        }
+
+        Category::create($validatedData);
+
+        return redirect('/dashboard/categories')->with('success', 'New category has been added!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('dashboard.admin.categories.show', [
+            'category' => $category,
+            'bibliographies' => Bibliography::where('category_id', $category->id)->get()
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('dashboard.admin.categories.edit', [
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255|unique:categories'
+        ]);
+
+        Category::where('id', $category->id)->update($validatedData);
+
+        return redirect('/dashboard/categories')->with('success', 'Category has been updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        Category::destroy($category->id);
+
+        return redirect('/dashboard/categories')->with('success', 'Category has been deleted!');
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,14 @@ class Category extends Model
 
     protected $guarded = ['id'];
 
+    public function scopeFilter($query, array $filters) {
+        $query->when($filters['search'] ?? false, function($query, $search) {
+            return $query->where(function($query) use ($search) {
+                $query->where('category_code', 'like', '%' . $search . '%')->orWhere('name', 'like', '%' . $search . '%');
+            });
+        });
+    }
+
     public function bibliography() {
         return $this->hasMany(Bibliography::class);
     }
